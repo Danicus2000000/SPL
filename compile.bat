e8bbe0f8d89cf53c48f5f92cf936a6f8fe3c2773
Dan <dbulman68@gmail.com>
2022-11-04 15:41:20 +0000

    Edited Batch File to allow full compile and fixed issues with it

--- compile.bat
+++ compile.bat
@@ -2,7 +2,9 @@
 flex spl.l
 bison spl.y
 gcc -o spl.exe spl.c spl.tab.c -lfl
-set /p infile=Please enter the file to parse: 
-set /p outfile=Please enter an output file or leave empty to output to console: 
-if "%~1"=="" (spl.exe < %infile%) else (spl.exe <%infile% > %outfile%)
+set /p infile=Please enter the file to parse (*.spl): 
+set outfile="none"
+set /p outfile=Please enter an output file or leave empty to output to console (*.c): 
+if %outfile%=="none" (echo Code Output: & spl.exe < %infile%) else (spl.exe <%infile% > %outfile% & echo GCC Output: & gcc -o %outfile%.exe %outfile% -lfl & echo Code Compile Output: & %outfile%.exe)
+echo.
 set /p quit=Press enter to exit
