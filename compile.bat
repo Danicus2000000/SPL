71876735508ff9101d9117fcff8415323dac34f5
Dan <dbulman68@gmail.com>
2022-11-04 16:19:48 +0000

    Updated compile.bat with more extensive tagging

--- compile.bat
+++ compile.bat
@@ -1,10 +1,13 @@
 @echo off
+echo Flex Output:
 flex spl.l
+echo Bison Output:
 bison spl.y
+echo GCC -SPL- Output:
 gcc -o spl.exe spl.c spl.tab.c -lfl
 set /p infile=Please enter the file to parse (*.spl): 
 set outfile="none"
 set /p outfile=Please enter an output file or leave empty to output to console (*.c): 
-if %outfile%=="none" (echo Code Output: & spl.exe < %infile%) else (spl.exe <%infile% > %outfile% & echo GCC Output: & gcc -o %outfile%.exe %outfile% -lfl & echo Code Compile Output: & %outfile%.exe)
+if %outfile%=="none" (echo Code Output: & spl.exe < %infile%) else (spl.exe <%infile% > %outfile% & echo GCC -code- Output: & gcc -o %outfile%.exe %outfile% -lfl & echo Code Compile Output: & %outfile%.exe)
 echo.
 set /p quit=Press enter to exit
